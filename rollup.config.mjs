a4a38737cd232f0c517ba1845383c85bae4e4118
artur <brugeman.artur@gmail.com>
2025-04-02 10:15:55 +0200

    Turn off CommonJS build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,11 +4,11 @@ import typescript from '@rollup/plugin-typescript'
 export default {
 	input: 'src/index.ts',
 	output: [
-		{
-			file: 'dist/index.cjs.js',
-			format: 'cjs',
-			sourcemap: true,
-		},
+		// {
+		// 	file: 'dist/index.cjs.js',
+		// 	format: 'cjs',
+		// 	sourcemap: true,
+		// },
 		{
 			file: 'dist/index.esm.js',
 			format: 'esm',
